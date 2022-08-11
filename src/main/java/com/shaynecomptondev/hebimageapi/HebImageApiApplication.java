6e23c5f6948315c8e3204e41ba004002daa17b11
shayne <shayne.compton@gmail.com>
2022-08-11 17:18:23 -0500

    init

---
+++ src/main/java/com/shaynecomptondev/hebimageapi/HebImageApiApplication.java
@@ -0,0 +1,13 @@
+package com.shaynecomptondev.hebimageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HebImageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HebImageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaynecomptondev/hebimageapi/HebImageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaynecomptondev.hebimageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HebImageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
