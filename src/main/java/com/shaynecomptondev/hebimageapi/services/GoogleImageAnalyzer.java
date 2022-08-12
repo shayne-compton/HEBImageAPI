f3401495d2a5337743053fbbf7301bd607bc39a5
shayne <shayne.compton@gmail.com>
2022-08-11 23:53:08 -0500

    Implement google image detection api call

--- src/main/java/com/shaynecomptondev/hebimageapi/services/GoogleImageAnalyzer.java
+++ src/main/java/com/shaynecomptondev/hebimageapi/services/GoogleImageAnalyzer.java
@@ -24,9 +24,61 @@ public class GoogleImageAnalyzer implements ImageAnalyzer {
     @Value("${GoogleVisionApiKey}")
     private String apiKey;
 
-
     @Override
     public Iterable<ImageObjectsDto> DetectObjects(byte[] content) {
-        return null;
+        if (content == null) {
+            throw new IllegalArgumentException("Content cannot be null");
+        }
+        String uri = imageUri + "?key=" + apiKey;
+        String encodedContent = Base64.getEncoder().encodeToString(content);
+        String jsonInputString = "{" +
+                "\"requests\":[" +
+                "{" +
+                "\"image\":{" +
+                "\"content\":\"" + encodedContent + "\"" +
+                "}," +
+                "\"features\":[" +
+                "{" +
+                "\"type\":\"LABEL_DETECTION\"," +
+                "\"maxResults\":10" +
+                "}" +
+                "]" +
+                "}" +
+                "]" +
+                "}";
+        try {
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(uri))
+                    .header("Accept", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonInputString))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+            String jsonResponse = response.body();
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            JsonNode node = mapper.readTree(jsonResponse);
+
+            GoogleImageDetectionResponseDto googleResponse = mapper.treeToValue(node, GoogleImageDetectionResponseDto.class);
+
+            List<ImageObjectsDto> detectedImages = new ArrayList<>();
+
+            ImageObjectsDto temp;
+            for (GoogleImageDetectionResponseDto.Response.LabelAnnotation annotation : googleResponse.responses[0].labelAnnotations)
+            {
+                temp = new ImageObjectsDto(annotation.description, annotation.score);
+                detectedImages.add(temp);
+            }
+            return detectedImages;
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
