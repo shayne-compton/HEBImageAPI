174f009f3c5867f992402162e7cb753cadffa82d
shayne <shayne.compton@gmail.com>
2022-08-11 23:54:59 -0500

    Implemented mapper functions

--- src/main/java/com/shaynecomptondev/hebimageapi/services/ImageServiceImpl.java
+++ src/main/java/com/shaynecomptondev/hebimageapi/services/ImageServiceImpl.java
@@ -1,19 +1,23 @@
 package com.shaynecomptondev.hebimageapi.services;
 
 import com.shaynecomptondev.hebimageapi.dtos.ImageDto;
+import com.shaynecomptondev.hebimageapi.dtos.ImageMetadataDto;
 import com.shaynecomptondev.hebimageapi.dtos.ImageObjectsDto;
 import com.shaynecomptondev.hebimageapi.dtos.ImageUploadDto;
 import com.shaynecomptondev.hebimageapi.entities.Image;
+import com.shaynecomptondev.hebimageapi.entities.ImageMetadata;
+import com.shaynecomptondev.hebimageapi.entities.ImageObject;
 import com.shaynecomptondev.hebimageapi.exceptions.ImageNotFoundException;
 import com.shaynecomptondev.hebimageapi.repositories.ImageRepository;
+import org.hibernate.boot.Metadata;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.*;
 
 
 public class ImageServiceImpl implements ImageService {
@@ -78,8 +82,8 @@ public class ImageServiceImpl implements ImageService {
             imageContents = image.getImage();
         }
 
-        //todo map imagedto to image entity
-        Image newImage = new Image();
+        //todo get image metadata
+        Iterable<ImageMetadataDto> imageMetadata = null;
 
         //Detect objects if specified
         Iterable<ImageObjectsDto> detectedObjects = null;
@@ -88,6 +92,7 @@ public class ImageServiceImpl implements ImageService {
             detectedObjects = imageAnalyzer.DetectObjects(imageContents);
         }
         //save our image
+        Image newImage = MapUploadToEntity(image, imageContents, detectedObjects, imageMetadata);
         newImage = imageRepository.save(newImage);
         ImageDto newImageDto = MapEntityToDto(newImage);
         return newImageDto;
@@ -109,7 +114,87 @@ public class ImageServiceImpl implements ImageService {
 
     private ImageDto MapEntityToDto(Image image) {
         ImageDto imageDto = new ImageDto();
-        //todo Add mapping operations. Maybe this function should go in a mapper class...
+        imageDto.setId(image.getId());
+        imageDto.setContent(image.getContent());
+        //map metadata
+        if (image.getImageMetadata() != null) {
+            ArrayList<ImageMetadataDto> metadataDtos = new ArrayList<>();
+            ImageMetadataDto tempMetadataDto = null;
+            for (ImageMetadata metadata : image.getImageMetadata()) {
+                tempMetadataDto = new ImageMetadataDto();
+                tempMetadataDto.setId(metadata.getId().toString());
+                tempMetadataDto.setName(metadata.getName());
+                tempMetadataDto.setValue(metadata.getValue());
+                metadataDtos.add(tempMetadataDto);
+            }
+            imageDto.setMetadata(metadataDtos.toArray(new ImageMetadataDto[metadataDtos.size()]));
+        }
+
+        //map objects
+        if (image.getImageObjects() != null) {
+            ArrayList<ImageObjectsDto> detectedObjectDtos = new ArrayList<>();
+            ImageObjectsDto tempImageObjectDto = null;
+            for (ImageObject imageObject : image.getImageObjects()) {
+                tempImageObjectDto = new ImageObjectsDto();
+                tempImageObjectDto.setId(imageObject.getId().toString());
+                tempImageObjectDto.setName(imageObject.getName());
+                tempImageObjectDto.setScore(imageObject.getScore().toString());
+                detectedObjectDtos.add(tempImageObjectDto);
+            }
+            imageDto.setDetectedObjects(detectedObjectDtos.toArray(new ImageObjectsDto[detectedObjectDtos.size()]));
+        }
         return imageDto;
+    }
+
+    private Image MapUploadToEntity(ImageUploadDto imageDto, byte[] imageContent, Iterable<ImageObjectsDto> detectedObjectDtos, Iterable<ImageMetadataDto> metadataDtos) {
+        Date today = new Date();
+        today.setHours(0);
+
+        //map image
+        Image image = new Image();
+        image.setContent(imageContent);
+        image.setActive(true);
+        image.setCreateDate(today);
+
+        String imageUrl = imageDto.getImageUrl();
+        if (imageUrl != null && imageDto.getImage() == null) {
+            image.setSource(imageUrl);
+        } else {
+            image.setSource("File");
+        }
+
+        //map metadata
+        Set<ImageMetadata> metadata = new HashSet<>();
+        ImageMetadata tempMetadata = null;
+        if (metadataDtos != null) {
+            for (ImageMetadataDto metadataDto : metadataDtos) {
+                tempMetadata = new ImageMetadata();
+                tempMetadata.setName(metadataDto.getName());
+                tempMetadata.setValue(metadataDto.getValue());
+                tempMetadata.setCreateDate(today);
+                tempMetadata.setActive(true);
+                tempMetadata.setImage(image);
+                metadata.add(tempMetadata);
+            }
+            image.setImageMetadata(metadata);
+        }
+
+        //map detected objects
+        Set<ImageObject> mappedObjects = new HashSet<>();
+        ImageObject tempImageObject = null;
+        if (detectedObjectDtos != null) {
+            for (ImageObjectsDto detectedObjectsDto : detectedObjectDtos) {
+                tempImageObject = new ImageObject();
+                tempImageObject.setName(detectedObjectsDto.getName());
+                tempImageObject.setScore(Double.parseDouble(detectedObjectsDto.getScore()));
+                tempImageObject.setCreateDate(today);
+                tempImageObject.setActive(true);
+                tempImageObject.setImage(image);
+                mappedObjects.add(tempImageObject);
+            }
+            image.setImageObjects(mappedObjects);
+        }
+
+        return image;
     }
 }
