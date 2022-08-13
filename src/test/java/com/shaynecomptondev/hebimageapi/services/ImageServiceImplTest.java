94310238ae451f9e3277b8dd457578023d6bb07e
shayne <shayne.compton@gmail.com>
2022-08-13 14:32:40 -0500

    Added/updated tests

--- src/test/java/com/shaynecomptondev/hebimageapi/services/ImageServiceImplTest.java
+++ src/test/java/com/shaynecomptondev/hebimageapi/services/ImageServiceImplTest.java
@@ -4,10 +4,11 @@ import com.shaynecomptondev.hebimageapi.dtos.ImageDto;
 import com.shaynecomptondev.hebimageapi.dtos.ImageObjectsDto;
 import com.shaynecomptondev.hebimageapi.dtos.ImageUploadDto;
 import com.shaynecomptondev.hebimageapi.entities.Image;
+import com.shaynecomptondev.hebimageapi.entities.ImageMetadata;
 import com.shaynecomptondev.hebimageapi.entities.ImageObject;
 import com.shaynecomptondev.hebimageapi.exceptions.ImageNotFoundException;
+import com.shaynecomptondev.hebimageapi.exceptions.InvalidParameterException;
 import com.shaynecomptondev.hebimageapi.repositories.ImageRepository;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,19 +16,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ImageServiceImplTest {
@@ -80,11 +76,13 @@ public class ImageServiceImplTest {
 
     @Test
     void getExistingImageById() {
-        Image image = createImage(1, "File", new Date(), true);
-        image.setId(1);
+        Date today = new Date();
+        HashSet<ImageMetadata> imageMetadata = new HashSet<>();
+        imageMetadata.add(ImageMetadata.builder().setName("Property").setValue("Value").setCreateDate(today).setId(1).setActive(true).build());
+        Image image = Image.builder().setId(1).setSource("File").setCreateDate(today).setActive(true).setImageMetadata(imageMetadata).setContent(getImageContent()).build();
         Mockito.when(imageRepository.findById(1)).thenReturn(Optional.of(image));
         ImageDto imageDto = imageService.GetImageById(1);
-        assertAll(  () -> assertNotNull(image),
+        assertAll(  () -> assertNotNull(imageDto),
                     () -> assertEquals(1, imageDto.getId()),
                     () -> assertNotNull(imageDto.getContent()));
     }
@@ -97,6 +95,17 @@ public class ImageServiceImplTest {
     }
 
     @Test
+    void GetImageByIdWithInvalidParameters() {
+        assertAll(  () -> assertThrows(InvalidParameterException.class, () -> {
+                        ImageDto imageDto = imageService.GetImageById(0);
+                    }),
+                    () -> assertThrows(InvalidParameterException.class, () -> {
+                        ImageDto imageDto = imageService.GetImageById(-1);
+                    })
+        );
+    }
+
+    @Test
     void saveImageFromUrlWithObjectDetection() {
         String mockUrl = "http://www.example.com/file.jpg";
         ImageUploadDto mockImageUploadDto = ImageUploadDto.builder()
@@ -105,7 +114,7 @@ public class ImageServiceImplTest {
                 .setEnableObjectDetection(true)
                 .build();
         Date today = new Date();
-        Image image = Image.builder().setId(1).setImageMetadata(null).setImageObjects(createImageObjects()).setContent(getImageContent()).setSource(mockUrl).setCreateDate(today).setActive(true).build();
+        Image image = Image.builder().setId(1).setImageMetadata(null).setImageObjects(createImageObjects()).setContent(getImageContent()).setLabel("dog").setSource(mockUrl).setCreateDate(today).setActive(true).build();
         Iterable<ImageObjectsDto> detectedObjects = createImageObjectsDto();
         Mockito.when(downloadService.DownloadFileFromUrl(mockUrl)).thenReturn(getImageContent());
         Mockito.when(imageRepository.save(Mockito.any(Image.class))).thenReturn(image);
@@ -117,7 +126,50 @@ public class ImageServiceImplTest {
                     () -> assertEquals(1, newImage.getId()),
                     () -> assertNotNull(newImage.getContent()),
                     () -> assertNotNull(newImage.getDetectedObjects()),
-                    () -> assertEquals(2, newImage.getDetectedObjects().length));
+                    () -> assertEquals(2, newImage.getDetectedObjects().length),
+                    () -> assertNotNull(newImage.getLabel()),
+                    () -> assertEquals("dog", newImage.getLabel()));
+    }
+
+    @Test
+    void saveImageFromFileWithEmptyLabelAndImageDetectionEnabled() {
+        String mockUrl = "http://www.example.com/file.jpg";
+        ImageUploadDto mockImageUploadDto = ImageUploadDto.builder()
+                .setImage(getImageContent())
+                .setEnableObjectDetection(true)
+                .build();
+
+        Date today = new Date();
+        Image image = Image.builder().setId(1).setImageMetadata(null).setImageObjects(createImageObjects()).setContent(getImageContent()).setLabel("dog").setSource(mockUrl).setCreateDate(today).setActive(true).build();
+        Iterable<ImageObjectsDto> detectedObjects = createImageObjectsDto();
+        Mockito.when(imageRepository.save(Mockito.any(Image.class))).thenReturn(image);
+        Mockito.when(imageAnalyzer.DetectObjects(Mockito.any(byte[].class))).thenReturn(detectedObjects);
+
+        ImageDto newImage = imageService.SaveImage(mockImageUploadDto);
+
+        assertAll(  () -> assertNotNull(newImage),
+                () -> assertEquals(1, newImage.getId()),
+                () -> assertNotNull(newImage.getContent()),
+                () -> assertNotNull(newImage.getDetectedObjects()),
+                () -> assertEquals(2, newImage.getDetectedObjects().length),
+                () -> assertNotNull(newImage.getLabel()),
+                () -> assertEquals("dog", newImage.getLabel()));
+    }
+
+    @Test
+    void saveImageWithInvalidParameters() {
+        ImageUploadDto multipleUploadSources = ImageUploadDto.builder().setImage(getImageContent()).setImageUrl("www.example.com/file.jpg").build();
+        ImageUploadDto sourceNotSpecified = ImageUploadDto.builder().setImageUrl(null).setImage(null).build();
+        assertAll(  () -> assertThrows(InvalidParameterException.class, () -> {
+                    imageService.SaveImage(null);
+                }),
+                () -> assertThrows(InvalidParameterException.class, () -> {
+                    imageService.SaveImage(multipleUploadSources);
+                }),
+                () -> assertThrows(InvalidParameterException.class, () -> {
+                    imageService.SaveImage(sourceNotSpecified);
+                })
+        );
     }
 
     private Image createImage(int id, String source, Date createDate, boolean isActive) {
